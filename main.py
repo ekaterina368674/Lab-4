d22569b0bfb4406ce71e77e4af8bb2f809aef628
ekaterina368674 <114882735+ekaterina368674@users.noreply.github.com>
2022-11-28 17:33:23 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,59 +1,100 @@
-import tkinter as tk
-from tkinter import messagebox
-from sqrEq import sqr_eq
+from tkinter import *
+from PIL import Image
+import pygame
+from random import *
+
+root = Tk()
+root.title('4 Лабораторная')
+root.geometry('540x310')
+root.resizable(True, True)
+file = "209473.gif"
+
+info = Image.open(file)
+frames = info.n_frames  # Считывание количества кадров гиф изображения
+
+im = [PhotoImage(file=file, format=f"gif -index {i}") for i in range(frames)]
+slovo = StringVar()
+
+count = 0
+anim = None
+pygame.mixer.init()
+up = 35
 
 
-def close():
-    window.destroy()
+def parol():  # Функция создания ключа
+    S = 'A B C D E F J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 0'.split()
+    password = ''
+    for i in range(3):
+        x = ''
+        d = 4
+        for i in range(4):
+            c = randrange(up - d)
+            if i == 0: d= 0
+            x += S[c]
+            d += c
+        password += x + '-'
+    login = Label(root, text= password[:-1:])
+    login.place(x=100, y=230, width=300, height=30)
+    Again = Button(root, text="try again", command=lambda: try_again(login))
+    Again.place(x=220, y=270, width=50, height=30)
 
 
-def calc():
-    A = float(arg_A.get())
-    B = float(arg_B.get())
-    C = float(arg_C.get())
-    if A == 0:
-        tk.messagebox.showerror('Error', 'Division by zero!')
-    else:
-        lbl_result.configure(text=sqr_eq(A, B, C))
+def play():  # Функция запуска песни
+    pygame.mixer.music.load('music.mp3')
+    pygame.mixer.music.play(loops=10)
 
 
-window = tk.Tk()
-window.title('Title')
-window.geometry('360x240')
-bg = tk.PhotoImage(file='gradient.png')
+def stop_music():  # Функция остановки песни
+    pygame.mixer.music.stop()
 
-frame = tk.Frame(window)
-frame.place(relx=0.5, rely=0.5, anchor='center')
 
-lbl_bg = tk.Label(frame, image=bg)
-lbl_bg.place(x=0, y=0)
+def animation(count):  # Функция запуска гифки
+    global anim
+    im2 = im[count]
+    gif_label.configure(image=im2)
+    count += 1
+    if count == frames:
+        count = 0
+    anim = root.after(130, lambda: animation(count))
 
-lbl_A = tk.Label(frame, text='A', font=('Arial', 30), bg='#000099', fg='#dddddd')
-lbl_A.grid(column=0, row=0, padx=10, pady=15)
-arg_A = tk.Entry(frame, width = 10)
-arg_A.insert(0, '1')
-arg_A.grid(column=0, row=1, padx=10, pady=15)
 
-lbl_B = tk.Label(frame, text='B', font=('Arial', 30))
-lbl_B.grid(column=1, row=0, padx=10, pady=15)
-arg_B = tk.Entry(frame, width = 10)
-arg_B.insert(0, '0')
-arg_B.grid(column=1, row=1, padx=10, pady=15)
+def stop_animation():  # Функция остановки гифки
+    global anim
+    root.after_cancel(anim)
 
-lbl_C = tk.Label(frame, text='C', font=('Arial', 30))
-lbl_C.grid(column=2, row=0, padx=10, pady=15)
-arg_C = tk.Entry(frame, width = 10)
-arg_C.insert(0, '0')
-arg_C.grid(column=2, row=1, padx=10, pady=15)
 
-lbl_roots = tk.Label(frame, text='Result:')
-lbl_roots.grid(column=0, row=2)
-lbl_result = tk.Label(frame, text='Enter the values.', font=('Arial', 10))
-lbl_result.grid(column=2, row=2)
+def change_button():  # Замена кнопки старт на кнопку стоп
+    start.destroy()
+    stop = Button(root, text="stop", command=lambda: [stop_animation(), stop_music(), change_second_button()])
+    stop.place(x=480, y=280, width=50, height=30)
 
-btn_calc = tk.Button(frame, text='Calculate', font=('Arial', 15), command=calc)
-btn_calc.grid(column=0, row=3)
-btn_exit = tk.Button(frame, text='Cancel', font=('Arial', 15), command=close)
-btn_exit.grid(column=2, row=3)
 
-window.mainloop()
+def change_second_button():  # Замена кнопки стоп на кнопку старт
+    stop.destroy()
+    start = Button(root, text="start", command=lambda: [animation(count), play(), change_button()])
+    start.place(x=480, y=280, width=50, height=30)
+
+
+def new_button():  # Создание кнопки активации программы
+    Click = Button(root, text="click", command=lambda: parol())
+    Click.place(x=20, y=280, width=50, height=30)
+
+
+def try_again(login):  # Удаление сгенерируемого кода и кнопки повтора, создание кнпоки начала алгоритма
+    login.destroy()
+    Again.destroy()
+    Click = Button(root, text="click", command=lambda: parol())
+    Click.place(x=20, y=280, width=50, height=30)
+
+
+gif_label = Label(root, image="")  # Открытие гифки
+gif_label.pack()
+
+start = Button(root, text="start", command=lambda: [animation(count), play(), change_button(), new_button()])
+start.place(x=250, y=150, width=50, height=30)
+Click = Button(root, text="click", command=lambda: parol())
+Again = Button(root, text="try again", command=lambda: try_again(login))
+stop = Button(root, text="stop", command=lambda: [stop_animation(), stop_music(), change_second_button()])
+login = Label(root, text='')
+
+root.mainloop()
